1c0967e9a35e974dba0384aac88e3a566a4ce1af
Myth197 <myth197.rando@gmail.com>
2026-01-18 02:59:38 -0600

    RAC3: Move Exclude locations to the correct group

--- worlds/rac3/rac3options.py
+++ worlds/rac3/rac3options.py
@@ -78,7 +78,6 @@ rac3_option_groups = [
         ProgressionBalancing,
         Accessibility,
         Deathlink,
-        RAC3ExcludeLocations,
     ]),
     OptionGroup("RAC3 Game Options", [
         IntroSkip,
@@ -112,6 +111,9 @@ rac3_option_groups = [
         ShipWings,
         ShipSkin,
         RatchetSkin,
+    ]),
+    OptionGroup("Item & Location Options", [
+        RAC3ExcludeLocations,
     ]),
 ]
 
